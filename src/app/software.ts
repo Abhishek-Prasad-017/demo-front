1b6dbaf1952d17e32eb94e4b61ff602d4f3c8c3f
Abhishek Prasad <abhishpr@cisco.com>
2020-02-27 22:12:22 +0530

    adding software(payload) structure

---
+++ src/app/software.ts
@@ -0,0 +1,15 @@
+export interface Software {
+  softwareId: string;
+  softwareName: string;
+  languageUsed: string;
+  createDate: string;
+  owner: string;
+  adminGroupName: string      
+	developerGroupName: string   
+	qaGroupName: string        	
+  releaseGroupName: string
+  asgName: string 	    
+	serviceName:  string 	    
+	designation:  string[] 	    
+	people: string[] 	    
+}
