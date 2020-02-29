27761609266a290cc60f6b4e48d4b2bff149e1ae
Abhishek Prasad <abhishpr@cisco.com>
2020-02-29 09:51:33 +0530

    adding bar-chart component

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -1,29 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { Label } from 'ng2-charts';
+import {Software} from '../software';
+
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.css']
 })
 export class BarChartComponent implements OnInit {
+  @Input() data:Software;
+  @Input() label:string[];
   public chartType: string = 'bar';
-
+  
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
   barChartOptions: ChartOptions = {
     responsive: true,
   };
-  barChartLabels: Label[] = ['Apple', 'Banana', 'Kiwifruit', 'Blueberry', 'Orange', 'Grapes'];
+  public options: any = {
+    barThickness: -1,
+    responsive: true
+  }
+  barChartLabels: Label[] = ["Chiranthan", "Manaswini", "Sandya" , "Yashika" , "Natrajan"]
   barChartType: ChartType = 'bar';
   barChartLegend = true;
   barChartPlugins = [];
-
+  barThickness: 1;
   barChartData: ChartDataSets[] = [
-    { data: [45, 37, 60, 70, 46, 33], label: 'Best Fruits' }
+    { data: [10, 7,9 , 3, 17], label: 'Softwares' }
   ];
   public chartClicked(e: any): void { }
   public chartHovered(e: any): void { }
